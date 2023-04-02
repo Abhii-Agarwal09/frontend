7cf2ece0009e5e44f91714d8c87da4b83eea2efe
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2023-04-02 22:24:47 +0530

    firebase config added

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { getStorage } from '@firebase/storage';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyDzyW4ZKp10nMjqnqi4pmz0I1dmNrze160',
+  authDomain: 'ezyfi-722cf.firebaseapp.com',
+  projectId: 'ezyfi-722cf',
+  storageBucket: 'ezyfi-722cf.appspot.com',
+  messagingSenderId: '592754353649',
+  appId: '1:592754353649:web:748ec5c3870edbf2a41050',
+  measurementId: 'G-P0DY06X2Z5',
 };
 
 export const app = initializeApp(firebaseConfig);
